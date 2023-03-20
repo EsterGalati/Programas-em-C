1002e654bd7420948babb3d007b7c82d3b744a7d
Ester <esterscgalati@gmail.com>
2023-03-20 19:41:27 -0300

    Jogo da Forca

---
+++ Jogo da Forca.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "%s");
+    int acertou=0;
+    int enforcou =1;
+    do
+    {
+        char chute;
+
+        printf("Qual letra? ");
+        scanf("%c", &chute);
+
+        for (int i = 0; i < strlen(palavrasecreta); i++)
+        {
+            if (palavrasecreta[i] == chute)
+            {
+                printf("A posição %d tem essa letra\n", i + 1);
+            }
+        }
+    } while (!acertou && !enforcou);
+
+    return 0;
+}
